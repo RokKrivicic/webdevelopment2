9b5148ff57d940852dc23c61b1168b32bbc2a5ec
Rok Krivicic <rokkrivicic@gmail.com>
2021-09-25 19:06:18 +0200

    Updated homework file

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -10,11 +10,11 @@ db.print_tables(verbose=True)
 db.pretty_print("SELECT Name FROM Artist;")
 
 #Print all data from the table Invoice where BillingCountry is Germany
-db.pretty_print("SELECT * FROM Invoice WHERE BillingCountry = 'Germany'")
+db.pretty_print("SELECT * FROM Invoice WHERE BillingCountry = 'Germany';")
 
 #Count how many albums are in the database. Look into the SQL documentation for help.
 # Hint: look for Min&Max and Count, Avg, Sum
 db.pretty_print("SELECT COUNT(AlbumId) AS 'Number of albums' FROM Album;")
 
 #How many customers are from France?
-db.pretty_print("SELECT COUNT(CustomerId) AS 'Number of costumer from France' FROM Customer WHERE Country = 'France'")
+db.pretty_print("SELECT COUNT(CustomerId) AS 'Number of costumer from France' FROM Customer WHERE Country = 'France';")
